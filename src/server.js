04349e125874f4af33bcf3c52b214a87e1e7e707
George Ijidola <geordola@gmail.com>
2022-10-24 16:41:03 +0100

    Restore migration and disable build in docker:start:dev

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const build = require('./app')
+const { tempMigration } = require('./db/tempMigration')
+const generateSocketDocs = require('./helpers/generateSocketDocs')
 
 const server = build()
 
 server.listen({ port: process.env.PORT, host: '::' }, async (err, address) => {
   try {
-    // await tempMigration()
-    // await generateSocketDocs()
+    await tempMigration()
+    await generateSocketDocs()
 
     if (err) throw err
 
